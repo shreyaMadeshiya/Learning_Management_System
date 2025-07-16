52184167905f5b240cd289f3c598dd3b885a5aba
shreyaMadeshiya <madeshiyashreya6@gmail.com>
2025-07-16 20:43:39 +0530

    added logout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route , Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
+import Login from './Pages/login';
 import NotFound from './Pages/NotFound';
 import Signup from './Pages/SignUp';
 
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element = {<HomePage />}></Route>
       <Route path="/about" element={<AboutUs/>}></Route>
       <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
       <Route path="*" element={<NotFound/>}></Route>
 
     </Routes>
--- client/src/Redux/Slices/AuthSlice.js
+++ client/src/Redux/Slices/AuthSlice.js
@@ -4,7 +4,7 @@ import { toast } from "react-hot-toast";
 import axiosInstance from "../../Helpers/axiosInstance";
 
 const initialState ={
-    isLoggedIn:localStorage.getItem('isLoggedIn') == 'true',
+    isLoggedIn:localStorage.getItem('isLoggedIn') === 'true',
     role: localStorage.getItem('role') || "",
     data: localStorage.getItem('data') || {}
 };
@@ -24,10 +24,57 @@ export const createAccount = createAsyncThunk("/auth/signup",async (data,{ rejec
     }
 })
 
+export const login = createAsyncThunk("/auth/login",async (data,{ rejectWithValue })=>{
+    try{
+        const res=axiosInstance.post("user/login",data)
+         toast.promise(res,{
+            loading:"Wait ! authenticaton in progress...",
+            success:(data)=> data?.data?.message || "Account created successfully",
+            error:"Failed to Login"
+         });
+         return (await res).data;
+    }
+    catch(error){
+          toast.error(error?.response?.data?.message || "Something went wrong");
+            return rejectWithValue(error?.response?.data);
+    }
+});
+export const logout= createAsyncThunk("/auth/logout",async()=>{
+     try{
+        const res=axiosInstance.post("user/logout")
+         toast.promise(res,{
+            loading:"Wait ! logout in progress...",
+            success:(data)=> data?.data?.message || "Account created successfully",
+            error:"Failed to logout"
+         });
+         return (await res).data;
+     }catch(error){
+        toast.error(error?.response?.data?.message);
+     }
+})
+
+
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers:{},
+    extraReducers: (builder) => {
+          builder
+          .addCase(login.fulfilled, (state,action) => {
+            localStorage.setItem("data",JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role",action?.payload?.user?.role);
+            state.isLoggedIn=true,
+            state.data=action?.payload?.user;
+            state.role=action?.payload?.user?.role;
+          })
+          .addCase(logout.fulfilled,(state)=>{
+            localStorage.clear();
+            state.data={};
+            state.isLoggedIn= false;
+            state.role="";
+          })
+    }
 });
 
 export default authSlice.reducer;
