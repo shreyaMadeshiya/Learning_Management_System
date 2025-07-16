88065ff97216ddffe2717abe28f33b6cfa0a7305
shreyaMadeshiya <madeshiyashreya6@gmail.com>
2025-07-17 00:30:06 +0530

    Completed course list page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Route , Routes } from 'react-router-dom';
 
 import AboutUs from './Pages/AboutUs';
+import CourseList from './Pages/Course/CourseList';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/login';
 import NotFound from './Pages/NotFound';
@@ -17,8 +18,12 @@ function App() {
     <Routes>
       <Route path="/" element = {<HomePage />}></Route>
       <Route path="/about" element={<AboutUs/>}></Route>
+      <Route path="/courses" element={<CourseList/>}></Route>cd
+
+
       <Route path="/signup" element={<Signup/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
+      
       <Route path="*" element={<NotFound/>}></Route>
 
     </Routes>
